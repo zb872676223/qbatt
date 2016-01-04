19dd6afb98c0dd36961debd924ef89be73e4343a
Andrei Andreyanau <areanum@gmail.com>
2016-01-04 10:34:56 +0300

    Keep the application running even if there is no active window
    Signed-off-by: Andrei Andreyanau <areanum@gmail.com>
    
    	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
-#include "qbattmain.h"
 #include <QApplication>
+#include "qbattmain.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	QBattMain w;
+
+	a.setQuitOnLastWindowClosed(false);
 
 	return a.exec();
 }
